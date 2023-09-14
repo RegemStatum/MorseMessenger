90a608874737dc43bbd9c491cf7af49912ff2d86
Oleksandr Kondratov <19samsung81k9@gmail.com@example.com>
2023-09-14 17:01:10 +0300

    Fix LoadingButton height is not equal to PrimaryButton in LoadingButton

--- app/_components/ui/buttons/LoadingButton.tsx
+++ app/_components/ui/buttons/LoadingButton.tsx
@@ -6,7 +6,7 @@ const LoadingButton: FC = () => {
   return (
     <PrimaryButton disabled>
       <div className="flex justify-center items-center gap-1">
-        <div className="w-[25px] h-[25px] text-gray-600">
+        <div className="w-[24px] h-[24px] text-gray-600">
           <Spinner />
         </div>
         <p className="text-gray-600 font-bold">Loading...</p>
